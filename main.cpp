886e8b17b17245179d0df1a31c53cbfc33634801
creeksuen <creeksuen@gmail.com>
2023-12-08 17:51:57 +0800

    ====last

--- main.cpp
+++ main.cpp
@@ -8,19 +8,9 @@
 #include <cstring>
 
 
-#define MAX(a, b) (((a)>=(b))? (a):(b))
-enum
-{
-    CC,
-    AA,
-};
+
 int main()
 {
-    int a = 0;
-    auto x = MAX(a, AA);
-    char buffer[20] ;
-    memset(buffer, '2', 20);
-    snprintf(buffer, 20, "11111111111111111111");
-    std::cout << x<< std::endl;
+
 
 }
